ffe216afcfd77d14b4d37f8ceac75a4042d6089b
Jean-marc Broutin <jmb@MacBook-Pro-de-Jean-marc.local>
2019-05-24 14:10:10 +0200

    Updates

---
+++ precache-manifest.28a691913b125ecc15eb76f6f02eb0c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73deb5f015a9e5d9c416ffaaab0fab7b",
+    "url": "/Chat/index.html"
+  },
+  {
+    "revision": "36038b2762df04f619c2",
+    "url": "/Chat/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d40cd72b72e9c1e2ffbe",
+    "url": "/Chat/static/css/main.14e4b161.chunk.css"
+  },
+  {
+    "revision": "36038b2762df04f619c2",
+    "url": "/Chat/static/js/2.341f31ec.chunk.js"
+  },
+  {
+    "revision": "d40cd72b72e9c1e2ffbe",
+    "url": "/Chat/static/js/main.ddb97a31.chunk.js"
+  },
+  {
+    "revision": "6c0716a284e20e5e162b",
+    "url": "/Chat/static/js/runtime~main.993c241a.js"
+  }
+]);
